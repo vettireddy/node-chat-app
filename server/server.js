d044b05c45952bc45308ccb0a67234e508eb56aa
venkat <sbellamkonda@innominds.com>
2018-01-10 15:42:46 +0530

    basic socket setup at both ends; FE and BE

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
-const express = require('express');
+const http = require('http');
 const path = require('path');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 var app = express();
-
+var server = http.createServer(app);
+var io = socketIO(server);
 //Middleware
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  socket.on('disconnect', () => {
+    console.log('Disconnected from Client');
+  })
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server running at port ${port}`);
 })
